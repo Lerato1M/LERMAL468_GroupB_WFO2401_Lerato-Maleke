f4b3517b5d5430a454ec27e6187d46918905d350
Lerato Maleke <154103356+Lerato1M@users.noreply.github.com>
2024-04-02 11:25:37 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 // Array of song objects. Add at least 5 songs with title, artist, and genre properties.
 const songs = [
     { title: "Hooked on a Feeling", artist: "Blue Swede", genre: "Pop" },
@@ -11,7 +12,15 @@ const songs = [
     { title: "Come and Get Your Love", artist: "Redbone", genre: "Rock" },
     { title: "I'm Not in Love", artist: "10cc", genre: "Pop" },
     { title: "Fooled Around and Fell in Love", artist: "Elvin Bishop", genre: "Rock" },
-    // Feel free to add even more songs
+    { title: "DO 4 LOVE", artist:"Snoh Aalegra", genre:"Pop" },
+    { title: "Something About Ya", artist:"J.Howell", genre:"R&B"},
+    { title: "So into you", artist:"Tamia", genre:"R&B"},
+    { title: "I will pray", artist:"Ebuka Songs", genre: "Gospel"},
+    { title: "Nkalakatha", artist:"Mandoza", genre:"Kwaito"},
+    { title: "Never would have made it", artist:"Marvin Sapp", genre:"Gospel"},
+    { title: "Mdlwembe", artist:"Zola", genre:"Kwaito"},
+    { title: "Ngithwale", artist:"Thulas and SG", genre: "Gospel"},
+    // This array has 16 songs
 ];
 
 
@@ -19,16 +28,21 @@ const songs = [
 const guardians = {
     "Star-Lord": "Rock",
     "Gamora": "Pop",
+    "Groot": "Kwaito",
+    "Drax": "Gospel",
+    "Rocket": "R&B",
     // Add preferences for Drax, Rocket, and Groot
 };
 
 // Function to generate playlist based on preferred genre
 function generatePlaylist(guardians, songs) {
     // Use the map() function to create playlists for each Guardian
-    // Your code here
+    for (let guardian in guardians) {
+        let playlist = songs.filter(song => song.genre === guardians[guardian]).map(song => song.title);
+        console.log(`${guardian}'s Playlist:`);
+        console.log(playlist);
+    }
 }
 
 // Call generatePlaylist and display the playlists for each Guardian
 generatePlaylist(guardians, songs);
-
-
